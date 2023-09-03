1f4286cd8498db6e90bcf0978ca81ae94da58582
hassan-dev51 <hassanmunir363@gmail.com>
2023-09-03 20:10:55 +0500

    working on navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const Navbar = () => {
+  const [home, setHome] = useState<boolean>(true);
+  const [about, setAbout] = useState<boolean>(false);
+  const [project, setProject] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const Home: HTMLElement | null = document.getElementById("home");
+      const TopHome: number | undefined = Home?.getBoundingClientRect().top;
+
+      const About: HTMLElement | null = document.getElementById("about");
+      const TopAbout: number | undefined = About?.getBoundingClientRect().top;
+
+      const Project: HTMLElement | null = document.getElementById("project");
+      const TopProject: number | undefined =
+        Project?.getBoundingClientRect().top;
+
+      if (TopHome && TopHome < 10) {
+        setHome(true);
+        setAbout(false);
+        setProject(false);
+      }
+      if (TopAbout && TopAbout < 10) {
+        setHome(false);
+        setAbout(true);
+        setProject(false);
+      }
+      if (TopProject && TopProject < 10) {
+        setHome(false);
+        setAbout(false);
+        setProject(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <nav className="fixed top-0 flex w-full items-center justify-between px-5 py-1">
+      <Image
+        src={"/images/logo.png"}
+        alt="logo"
+        width={380}
+        height={380}
+        priority
+        className="h-[65px] w-auto object-contain"
+      />
+      <ul>
+        <li className="space-x-5 font-semibold uppercase">
+          <a
+            href="#home"
+            className={home ? `text-[#2493D4]` : `text-[#68696E]`}
+          >
+            Home
+          </a>
+          <a
+            href="#about"
+            className={about ? `text-[#2493D4]` : `text-[#68696E]`}
+          >
+            About
+          </a>
+          <a
+            href="#project"
+            className={project ? `text-[#2493D4]` : `text-[#68696E]`}
+          >
+            Projects
+          </a>
+        </li>
+      </ul>
+
+      <button className="rounded-full border px-4 py-2 font-semibold text-[#68696E] hover:text-[#2493d4]">
+        <a href="#contact">Contact</a>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ components/Projects.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Projects = (props: Props) => {
+  return <div id="project">Projects</div>;
+};
+
+export default Projects;
